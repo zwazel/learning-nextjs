50fa18cc9f4fe750958bf6a8b779f907782dfb94
Tim Irmler <zwazel@hotmail.com>
2021-10-10 17:59:15 +0200

    i'm so confused lol

--- pages/image_gallery/imageGallery.js
+++ pages/image_gallery/imageGallery.js
@@ -2,11 +2,11 @@ import Layout, {siteTitle} from '../../components/layout'
 import Head from "next/head";
 import {getImages} from "../../lib/imageGalleryUtil";
 import utilStyles from "../../styles/utils.module.css";
+import galleryStyle from "../../styles/gallery.module.css";
 import Image from 'next/image'
 
 export async function getStaticProps() {
     const allImagesData = getImages()
-    console.log(allImagesData)
     return {
         props: {
             allImagesData
@@ -23,11 +23,11 @@ export default function ImageGallery({allImagesData}) {
 
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
-                <div>
+                <div className={galleryStyle.galleryContainer}>
                     {allImagesData.map(({id, fileName}) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <Image src={'/gallery/'+fileName} alt={id} layout={"fill"}/>
-                        </li>
+                        <div className={galleryStyle.galleryItemContainer} key={id}>
+                            <Image src={'/gallery/' + fileName} alt={id} layout={"fill"}/>
+                        </div>
                     ))}
                 </div>
             </section>
