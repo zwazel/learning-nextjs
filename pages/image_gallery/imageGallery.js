32a1b1cd5360d3dc64f9882bddd6c9444c2e97e2
Tim Irmler <zwazel@hotmail.com>
2021-10-14 11:13:05 +0200

    ich gibs glaub uf lol

--- pages/image_gallery/imageGallery.js
+++ pages/image_gallery/imageGallery.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import {getImages} from "../../lib/imageGalleryUtil";
 import utilStyles from "../../styles/utils.module.css";
 import galleryStyle from "../../styles/gallery.module.css";
+import {Box, Container, Text, Wrap, WrapItem} from "@chakra-ui/react";
 import Image from 'next/image'
 
 export async function getStaticProps() {
@@ -16,17 +17,35 @@ export async function getStaticProps() {
 
 export default function ImageGallery({allImagesData}) {
     return (
-        <Layout>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
+        <Box overflow="hidden" bg="purple.100" minH="100vh">
+            <Container>
+                <Text
+                    color="pink.800"
+                    fontWeight="semibold"
+                    mb="1rem"
+                    textAlign="center"
+                    textDecoration="underline"
+                    fontSize={["4xl", "4xl", "5xl", "5xl"]}
+                >
+                    NextJS Image Gallery
+                </Text>
+            </Container>
 
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={utilStyles.headingLg}>Blog</h2>
+            <Wrap px="1rem" spacing={4} justify="center">
                 {allImagesData.map(({id, fileName}) => (
-                    <Image src={'/gallery/' + fileName} key={id} alt={id} width={600} height={400}/>
+                    <WrapItem
+                        key={id}
+                        boxShadow="base"
+                        rounded="20px"
+                        overflow="hidden"
+                        bg="white"
+                        lineHeight="0"
+                        _hover={{boxShadow: "dark-lg"}}
+                    >
+                        <Image src={'/gallery/' + fileName} key={id} alt={id} width={600} height={400}/>
+                    </WrapItem>
                 ))}
-            </section>
-        </Layout>
+            </Wrap>
+        </Box>
     )
 }
